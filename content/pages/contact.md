b8db941e0d6aea80ce0d2f0588802da7854f1d59
Alessio Koci <ko.alessio@gmail.com>
2020-03-14 19:49:15 +0100

    feat: add projects page

--- content/pages/contact.md
+++ content/pages/contact.md
@@ -7,7 +7,7 @@ template: page
 - **Email**: [ko{DOT}alessio[AT]gmail{DOT}com](mailto:ko{DOT}alessio[AT]gmail{DOT}com)
 - **GitHub**: [alewin](https://github.com/alewin)
 - **Linkldn**: [alessiokoci](https://www.linkedin.com/in/alessiokoci/)
-
+- **Twitter**: [alessiokoci](https://twitter.com/alessiokoci)
 
 <div class="centered-iframe">
 <!-- Begin Mailchimp Signup Form -->
---
+++ src/pages/projects.js
@@ -0,0 +1,47 @@
+/* eslint-disable class-methods-use-this */
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+import ProjectListing from '../components/ProjectListing'
+import Layout from '../layout'
+import config from '../../data/SiteConfig'
+import projects from '../../data/projects'
+
+
+export default class PublicationsPage extends Component {
+  get activeProjects() {
+    return projects.filter(item => item.active)
+  }
+
+  get inactiveProjects() {
+    return projects.filter(item => !item.active)
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Helmet title={`Published Articles – ${config.siteTitle}`} />
+        <div className="container">
+          <header className="page-header">
+            <h1>Active Projects</h1>
+          </header>
+          <div className="page">
+            <ProjectListing projects={this.activeProjects} />
+          </div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <div className="container">
+          <header className="page-header">
+            <h1>Inactive Projects</h1>
+          </header>
+          <div className="page">
+            <ProjectListing
+              projects={this.inactiveProjects}
+            />
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
